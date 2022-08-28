e2eb491580a518ab371b55b56eaca09c21e521c6
Andria Kilasonia <akila19@freeuni.edu.ge>
2022-08-28 20:26:01 +0400

    implement CPU architecture classes for accurate simulation

--- projects/finalproject_hack_simulator/n2t/core/hack_simulator/facade.py
+++ projects/finalproject_hack_simulator/n2t/core/hack_simulator/facade.py
@@ -1,9 +1,38 @@
 from __future__ import annotations
+from dataclasses import dataclass, field
 
 from typing import Iterable
 
 
+@dataclass
+class Registers:
+    PC: int = 0
+    D: int = 0
+    A: int = 0
+    M: int = 0
+
+
+@dataclass
+class Ram:
+    size: int = 2**15
+    values: dict[int, int] = field(default_factory=lambda: {})
+
+    def set(self, address: int, value: int) -> None:
+        assert address < self.size
+        
+        self.values[address] = value
+
+    def get(self, address: int) -> int:
+        assert address < self.size
+
+        return self.values.get(address, 0)
+
+
+@dataclass
 class HackSimulator:
+    registers: Registers = field(default_factory=Registers)
+    ram: Ram = field(default_factory=Ram)
+
     @classmethod
     def create(cls) -> HackSimulator:
         return cls()
